b014d768d27cb97df6587c39466f1b993e714f71
marceldavidbaroi <marceldavidbaroi@gmail.com>
2026-07-31 01:43:53 +0600

    mono repo build issue fixed

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,20 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+// The workspace root is one level up from the mobile directory
+const workspaceRoot = path.resolve(projectRoot, '..');
+
+const config = getDefaultConfig(projectRoot);
+
+// Add the monorepo workspace root to watch folders
+config.watchFolders = [workspaceRoot];
+
+// Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
 
 module.exports = withNativeWind(config, { input: './global.css' });
